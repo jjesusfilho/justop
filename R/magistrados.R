6d0a36478ad33961093d25ffc4e20be902b0297d
José de Jesus Filho <jjesusfilho@gmail.com>
2022-01-13 17:40:51 -0300

    atualiza base de magistrados

---
+++ R/magistrados.R
@@ -0,0 +1,10 @@
+#' @title magistrados
+#' @description nomes e sobrenomes dos magistrados de
+#'     todos os tribunais estaduais e federais
+#' @format dataframe com duas colunas:
+#' \describe{
+#'   \item{\code{tribunal}}{sigla do tribunal}
+#'   \item{\code{word}}{nome ou sobrenome}
+#' }
+#' @source \url{https://www.cnj.jus.br/corregedoria/justica_aberta/?}
+"magistrados"
