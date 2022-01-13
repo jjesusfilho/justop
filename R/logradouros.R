3bb6f80fd43a1ce771cf0cc1f085e5ae746e2a77
José de Jesus Filho <jjesusfilho@gmail.com>
2022-01-13 19:22:33 -0300

    Adiciona logradouros

---
+++ R/logradouros.R
@@ -0,0 +1,8 @@
+#' @title logradouros
+#' @description Tipos de logradouros
+#' @format dataframe com uma coluna:
+#' \describe{
+#'   \item{\code{word}}{tipo de logradouro}
+#' }
+#' @source \url{https://gist.github.com/jjesusfilho/10524df296794b2f4673b42b4f8c8c3a}
+"logradouros"
